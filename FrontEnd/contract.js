fcdd41925a1f14a257acd9eb13aeb43e1ead20f9
simonete1 <simonete1@s2.rpn.ch>
2026-02-13 14:46:16 +0100

    ajout de code dans la const abi donné par copilot le code s'execute correctement a vérifier si c'est le bon code qui est mis etpourquoi il est pas dans le fichier ou je copier colle de base

--- FrontEnd/contract.js
+++ FrontEnd/contract.js
@@ -108,4 +108,37 @@ export const abi = [
         "stateMutability": "view",
         "type": "function"
     }
+    ,
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "nombreLingots",
+                "type": "uint256"
+            }
+        ],
+        "name": "meltIron",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "nombreLingots",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "nouveauStockMelted",
+                "type": "uint256"
+            }
+        ],
+        "name": "Melted",
+        "type": "event"
+    }
 ]
